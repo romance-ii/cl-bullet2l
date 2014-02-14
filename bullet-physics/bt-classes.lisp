219d36c21bcb834c88af8cadc893fae684fdde4b
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2014-02-14 14:01:25 -0500

    start using a common base class

--- bullet-physics/bt-classes.lisp
+++ bullet-physics/bt-classes.lisp
@@ -2,219 +2,222 @@
 
 ;;; Worlds
 
-(defclass collision-world ()
+(defclass /c++-class/ ()
   ((ff-pointer :reader ff-pointer)))
 
-(defclass discrete-dynamics-world ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass collision-world (/c++-class/)
+  ())
 
-(defclass simple-dynamics-world ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass discrete-dynamics-world (/c++-class/)
+  ())
+
+(defclass simple-dynamics-world (/c++-class/)
+  ())
 
 ;;; Things in worlds (objects/bodies)
 
-(defclass collision-object ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass collision-object (/c++-class/)
+  ())
 
-(defclass box-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass box-shape (/c++-class/)
+  ())
 
-(defclass sphere-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass sphere-shape (/c++-class/)
+  ())
 
-(defclass capsule-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass capsule-shape (/c++-class/)
+  ())
 
 (defclass capsule-shape-x (capsule-shape)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
 (defclass capsule-shape-z (capsule-shape)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
-(defclass cylinder-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass cylinder-shape (/c++-class/)
+  ())
 
 (defclass cylinder-shape-x (cylinder-shape)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
 (defclass cylinder-shape-z (cylinder-shape)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
-(defclass cone-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass cone-shape (/c++-class/)
+  ())
 
 (defclass cone-shape-x (cone-shape)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
 (defclass cone-shape-z (cone-shape)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
-(defclass static-plane-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass static-plane-shape (/c++-class/)
+  ())
 
-(defclass convex-hull-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass convex-hull-shape (/c++-class/)
+  ())
 
-(defclass triangle-mesh ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass triangle-mesh (/c++-class/)
+  ())
 
-(defclass convex-triangle-mesh-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass convex-triangle-mesh-shape (/c++-class/)
+  ())
 
-(defclass bvh-triangle-mesh-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass bvh-triangle-mesh-shape (/c++-class/)
+  ())
 
-(defclass scaled-bvh-triangle-mesh-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass scaled-bvh-triangle-mesh-shape (/c++-class/)
+  ())
 
-(defclass triangle-mesh-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass triangle-mesh-shape (/c++-class/)
+  ())
 
-(defclass triangle-index-vertex-array ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass triangle-index-vertex-array (/c++-class/)
+  ())
 
-(defclass compound-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass compound-shape (/c++-class/)
+  ())
 
 ;;; Constraints
 
 ;;; Vectors, matrices, et al.
 
-(defclass vector3 ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass vector3 (/c++-class/)
+  ())
 
 (defclass vector4 (vector3)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
-(defclass quaternion ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass quaternion (/c++-class/)
+  ())
 
-(defclass matrix-3x3 ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass matrix-3x3 (/c++-class/)
+  ())
 
-(defclass transform ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass transform (/c++-class/)
+  ())
 
 ;;; Unsorted mass of things
 
-(defclass motion-state ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass motion-state (/c++-class/)
+  ())
 
-(defclass bu-simplex1to4 ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass bu-simplex1to4 (/c++-class/)
+  ())
 
-(defclass empty-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass empty-shape (/c++-class/)
+  ())
 
-(defclass multi-sphere-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass multi-sphere-shape (/c++-class/)
+  ())
 
-(defclass uniform-scaling-shape ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass uniform-scaling-shape (/c++-class/)
+  ())
 
-(defclass sphere-sphere-collision-algorithm ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass sphere-sphere-collision-algorithm (/c++-class/)
+  ())
 
-(defclass default-collision-configuration ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass default-collision-configuration (/c++-class/)
+  ())
 
-(defclass collision-dispatcher ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass collision-dispatcher (/c++-class/)
+  ())
 
-(defclass simple-broadphase ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass simple-broadphase (/c++-class/)
+  ())
 
-(defclass axis-sweep3 ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass axis-sweep3 (/c++-class/)
+  ())
 
-(defclass bt-32-bit-axis-sweep3 ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass bt-32-bit-axis-sweep3 (/c++-class/)
+  ())
 
-(defclass multi-sap-broadphase ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass multi-sap-broadphase (/c++-class/)
+  ())
 
-(defclass clock ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass clock (/c++-class/)
+  ())
 
-(defclass cprofile-node ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass cprofile-node (/c++-class/)
+  ())
 
-(defclass cprofile-iterator ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass cprofile-iterator (/c++-class/)
+  ())
 
-(defclass cprofile-manager ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass cprofile-manager (/c++-class/)
+  ())
 
-(defclass cprofile-sample ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass cprofile-sample (/c++-class/)
+  ())
 
-(defclass idebug-draw ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass idebug-draw (/c++-class/)
+  ())
 
-(defclass chunk ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass chunk (/c++-class/)
+  ())
 
-(defclass serializer ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass serializer (/c++-class/)
+  ())
 
 (defclass default-serializer (serializer)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
-(defclass discrete-dynamics-world ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass discrete-dynamics-world (/c++-class/)
+  ())
 
-(defclass simple-dynamics-world ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass simple-dynamics-world (/c++-class/)
+  ())
 
 (defclass rigid-body (collision-object)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
 (defclass typed-constraint (typed-object)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
-(defclass angular-limit ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass angular-limit (/c++-class/)
+  ())
 
 (defclass point->point-constraint (typed-constraint)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
 (defclass hinge-constraint (typed-constraint)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
 (warn "Guessing at HINGE-2-CONSTRAINT")
 (defclass hinge-2-constraint (hinge-constraint)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
 (defclass cone-twist-constraint (typed-constraint)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
-(defclass rotational-limit-motor ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass rotational-limit-motor (/c++-class/)
+  ())
 
-(defclass translational-limit-motor ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass translational-limit-motor (/c++-class/)
+  ())
 
 (defclass generic-6-dof-constraint (typed-constraint)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
 (defclass slider-constraint (typed-constraint)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
 (defclass generic-6-dof-spring-constraint (generic-6dof-constraint)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
 (defclass universal-constraint (generic-6dof-constraint)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
 (defclass hinge2-constraint (generic-6dof-spring-constraint)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
 (defclass gear-constraint (typed-constraint)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
 (defclass fixed-constraint (typed-constraint)
-  ((ff-pointer :reader ff-pointer)))
+  ())
 
-(defclass sequential-impulse-constraint-solver ()
-  ((ff-pointer :reader ff-pointer)))
+(defclass sequential-impulse-constraint-solver (/c++-class/)
+  ())
 
